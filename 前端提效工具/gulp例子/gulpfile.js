d05d1c4abf0c29c35aa7fcdcfbbd2ad67a477e88
zouwq <zouwq@mingyuanyun.com>
2019-07-30 14:20:13 +0800

    gulp 完成

--- 前端提效工具/gulp例子/gulpfile.js
+++ 前端提效工具/gulp例子/gulpfile.js
@@ -11,18 +11,19 @@ gulp.task('serve', function() {
         server: {
             baseDir: "./"
         },
+        host:'http://127.0.0.1',
         port: 3033
     });
     // 监听某一个文件夹下的所有scss文件，执行sass任务
-    gulp.watch('./src/*.html').on('change', reload);;
-    gulp.watch('./src/css/*.less', ['less']);
+    gulp.watch('./app/*.html').on('change', reload);;
+    gulp.watch('./app/less/*.less', ['less']);
 });
 
 
 gulp.task('less', function() {
-    return gulp.src('./src/css/*.less') // 源文件
+    return gulp.src('./app/less/*.less') // 源文件
             .pipe(less()) // 执行方法
-            .pipe(gulp.dest('./src/css')) // 输出
+            .pipe(gulp.dest('./app/')) // 输出
             .pipe(reload({stream: true})) // 当scss文件更新后触发页面更新
 })
 
